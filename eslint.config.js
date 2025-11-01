15c04acd4e164e372c26615451f91503c921ccb5
Boran Uzun <boran.u3@gmail.com>
2025-11-01 01:04:31 +0100

    chore(eslint): migrate to ESLint v9 flat config and remove deprecated files

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import astro from "eslint-plugin-astro";
+
+/** @type {import("eslint").Linter.FlatConfig[]} */
+export default [
+  {
+    ignores: [".vscode/", "dist/", "node_modules/", "public/"],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...astro.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      semi: ["error", "always"],
+      quotes: ["error", "double", { allowTemplateLiterals: true }],
+      "@typescript-eslint/triple-slash-reference": "off",
+    },
+  },
+  {
+    files: ["**/*.astro"],
+    languageOptions: {
+      parser: astro.parser,
+      parserOptions: {
+        parser: tseslint.parser,
+        extraFileExtensions: [".astro"],
+      },
+    },
+  },
+];
